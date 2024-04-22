c5919d393274a7fd8eb966359fcba45414b197ee
Ina Burström <inaburstrom@hotmail.com>
2024-04-22 10:06:17 +0200

    Initial Commit

---
+++ HabitsExam/HabitsExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsExamApp.swift
+//  HabitsExam
+//
+//  Created by Ina Burström on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct HabitsExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
