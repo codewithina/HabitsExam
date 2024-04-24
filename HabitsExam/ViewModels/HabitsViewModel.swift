eba277f5d7362e5972b3649d272315ea5ccc17b6
Ina Burström <inaburstrom@hotmail.com>
2024-04-24 10:42:49 +0200

    Added more example habits.

--- HabitsExam/ViewModels/HabitsViewModel.swift
+++ HabitsExam/ViewModels/HabitsViewModel.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 class HabitsViewModel: ObservableObject {
     @Published var habits: [Habit] = [
-        Habit(name: "Läsa", description: "Läs en bok i 20 minuter varje dag", isCompleted: false),
-            Habit(name: "Jogga", description: "Jogga 3 km varje morgon", isCompleted: false),
-            Habit(name: "Meditation", description: "Meditera 15 minuter varje kväll", isCompleted: false)
-        ]
+        Habit(name: "Läsa", description: "Läs en bok i 20 minuter varje dag", isCompleted: false, completedDates: []),
+        Habit(name: "Jogga", description: "Jogga 3 km varje morgon", isCompleted: false, completedDates: []),
+        Habit(name: "Meditation", description: "Meditera 15 minuter varje kväll", isCompleted: false, completedDates: []),
+        Habit(name: "Yoga", description: "Gör en 30 minuters yogaövning varje eftermiddag", isCompleted: false, completedDates: []),
+        Habit(name: "Grönsaker", description: "Ät minst en portion grönsaker till varje måltid", isCompleted: false, completedDates: []),
+        Habit(name: "Skriv dagbok", description: "Skriv i din dagbok varje kväll innan du går och lägger dig", isCompleted: false, completedDates: []),
+        Habit(name: "Dricka grönt te", description: "Drick en kopp grönt te varje morgon", isCompleted: false, completedDates: []),
+        Habit(name: "Stretching", description: "Gör en kort stretchingrutin varje morgon för att vakna upp kroppen", isCompleted: false, completedDates: []),
+        Habit(name: "Planera nästa dag", description: "Planera dina uppgifter och mål för nästa dag varje kväll", isCompleted: false, completedDates: [])
+    ]
     
     func addHabit(habit: Habit){
         habits.append(habit)
